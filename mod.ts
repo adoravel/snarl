c1971265c7f33a0aac490e817ef042c948682c50
july <w@stay.rip>
2026-01-25 01:38:10 -0300

    fix: jsx type safety
    chore: version dump

--- mod.ts
+++ mod.ts
@@ -1,3 +1,10 @@
+/// <reference types="@july/snarl/jsx-runtime" />
+
+/**
+ * snarl, a minimal web framework for deno
+ * @module
+ */
+
 /**
  * Copyright (c) 2025 adoravel
  * SPDX-License-Identifier: LGPL-3.0-or-later
@@ -8,3 +15,55 @@ export * from "./router.ts";
 export * from "./utils.ts";
 export * from "./cookie.ts";
 export * from "./stream.ts";
+
+import type { Component } from "@july/snarl/jsx-runtime";
+
+type CSSProperties =
+	& {
+		[K in keyof CSSStyleDeclaration]?: CSSStyleDeclaration[K] extends string ? string | number
+			: never;
+	}
+	& {
+		[key: `--${string}`]: string | number;
+		[key: `-webkit-${string}`]: string | number;
+		[key: `-moz-${string}`]: string | number;
+		[key: `-ms-${string}`]: string | number;
+	};
+
+type HTMLAttributeMap<T = HTMLElement> = Partial<
+	Omit<T, keyof Element | "children" | "style" | "href"> & {
+		style?: string | CSSProperties;
+		class?: string;
+		dangerouslySetInnerHTML?: { __html: string };
+		children?: any;
+		href: string | SVGAnimatedString;
+		[key: `data-${string}`]: string | number | boolean | null | undefined;
+		[key: `aria-${string}`]: string | number | boolean | null | undefined;
+		[key: `on${string}`]: string | ((e: Event) => void);
+	}
+>;
+
+declare global {
+	namespace JSX {
+		export type ElementType =
+			| keyof IntrinsicElements
+			| Component<any>;
+
+		export interface ElementChildrenAttribute {
+			// deno-lint-ignore ban-types
+			children: {};
+		}
+
+		export type IntrinsicElements =
+			& {
+				[K in keyof HTMLElementTagNameMap]: HTMLAttributeMap<
+					HTMLElementTagNameMap[K]
+				>;
+			}
+			& {
+				[K in keyof SVGElementTagNameMap]: HTMLAttributeMap<
+					SVGElementTagNameMap[K]
+				>;
+			};
+	}
+}
